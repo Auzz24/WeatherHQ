7269b758a995fa044a62c9441264ebdbd2dda2f4
Austin Miller <austin.miller1424@gmail.com>
2022-03-21 13:20:33 -0500

    wetather/geocoding function combined

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var inputCity = document.querySelector("#cityName")
 
-function latLon (city){
+function searchCity (city){
     fetch ("http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&appid=b3353fc64354c9fbf62bd339178f2411")
 
     .then(response =>{
@@ -12,54 +12,30 @@ function latLon (city){
         var lat = response[0].lat
         var lon = response[0].lon 
 
-        var myData={
-            lat: lat,
-            lon: lon
-        }
-        console.log(myData);
-        localStorage.setItem("mySavedData", JSON.stringify(myData))
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&appid=b3353fc64354c9fbf62bd339178f2411")
+        .then (response =>{
+            return response.json()
+        })
+        .then(response =>{
+            console.log(response);
+            
+        var current = response.current.weather[0].main 
+        var temp = response.current.temp
+        var uvi = response.current.uvi
+        var humidity = response.current.humidity
+        var wind = response.current.wind_speed
+
+        })
+
     })
     .catch(err => {
         console.error(err);
     })
 }
 
-function getData (lat, lon){
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=b3353fc64354c9fbf62bd339178f2411")
-
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        console.log(response);
-        // var totalCases = response.actuals.cases 
-  
-
-        // document.querySelector("#totalDeaths").textContent = totalDeaths
-        
-
-    //     var myData = {
-    //         totalCases: totalCases
-    //    }
-       
-    //    localStorage.setItem("mySavedData", JSON.stringify())
-    })
-    .catch(err => {
-        console.error(err);
-    });
-}
 
 searchButton.addEventListener("click", function(){
-    latLon(inputCity.value)
-    console.log(inputCity.value)
+    searchCity(inputCity.value) 
     });
 
-    // searchButton.addEventListener("click", function (){
-    //     getData(lat, lon)
-    //     }
-    //     );
-    
-    // searchButton.addEventListener("click", function(){
-    //     getData(inputState.value)
-    //     })
-        
+   
